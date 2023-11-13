7b2a4e70d05120002907f5c700c6f9bad93637ba
Tormod G. Hellen <tormod.hellen@gmail.com>
2023-11-13 15:14:34 +0100

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-use cargo_metadata::MetadataCommand;
+use core::fmt;
+
+use cargo_metadata::{MetadataCommand, Package};
 
 struct PackageHealth {
+    name: String,
     maintainers_last_month: i32,
+}
+
+impl PackageHealth {
+    pub fn score(&self) -> f32 {
+        self.maintainers_last_month as f32
+    }
+}
+
+impl fmt::Display for PackageHealth {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}\n  maintainers: {}", self.name, self.maintainers_last_month)
+    }
 }
 
 fn main() {
     let metadata = MetadataCommand::new().exec().unwrap();
-    metadata.packages;
+    let mut healths = metadata.packages
+        .iter()
+        .map(health_of_package)
+        .collect::<Vec<_>>();
+    healths.sort_by(|pa, pb| pa.score().partial_cmp(&pb.score()).unwrap());
+    healths
+        .into_iter()
+        .for_each(|ph| println!("{}", ph));
+}
+
+fn health_of_package(pkg: &Package) -> PackageHealth {
+    println!("{:?}", pkg.authors);
+    PackageHealth { name: pkg.name.to_owned(), maintainers_last_month: 2 }
 }
