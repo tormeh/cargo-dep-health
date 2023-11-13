d5f1fd8e5ab1fa97f460286df06738521dd03930
Tormod G. Hellen <tormod.hellen@gmail.com>
2023-11-13 14:14:16 +0100

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use cargo::{ops, core::{Package, Workspace}};
+
 fn main() {
-    println!("Hello, world!");
+    let ws = Workspace::new()?;
+    let package_set = ops::resolve_ws(ws)?.0;
+    let package_statuses = package_set.packages().map(status_of_package).collect();
+}
+
+fn status_of_package(pkg: &Package) -> String {
+
 }
