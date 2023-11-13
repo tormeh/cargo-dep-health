73a6a9fc4608f29b1da5b0094871fe072bccce65
Tormod G. Hellen <tormod.hellen@gmail.com>
2023-11-13 14:39:29 +0100

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use cargo_metadata::MetadataCommand;
+
+struct PackageHealth {
+    maintainers_last_month: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let metadata = MetadataCommand::new().exec().unwrap();
+    metadata.packages;
 }
